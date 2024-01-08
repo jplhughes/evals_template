de75124770e0294dab5e28e7617bf6b1645e7026
John Hughes <jpl.hughes@btinternet.com>
2024-01-08 09:59:04 +0000

    cleanup inference api

--- evals/apis/inference/api.py
+++ evals/apis/inference/api.py
@@ -1,18 +1,18 @@
 import asyncio
 import logging
+from collections import defaultdict
 from itertools import chain
 from pathlib import Path
 from typing import Callable, Literal, Union
-from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from evals.apis.inference.anthropic import ANTHROPIC_MODELS, AnthropicChatModel
+from evals.apis.inference.model import InferenceAPIModel, LLMResponse
 from evals.apis.inference.openai.chat import OpenAIChatModel
 from evals.apis.inference.openai.completion import OpenAICompletionModel
 from evals.apis.inference.openai.utils import COMPLETION_MODELS, GPT_CHAT_MODELS
-from evals.apis.inference.model import InferenceAPIModel, LLMResponse
 from evals.data_models.messages import Prompt
 from evals.utils import load_secrets, setup_environment
 
@@ -75,6 +75,52 @@ class InferenceAPI:
         self.model_timings = {}
         self.model_wait_times = {}
 
+    def model_id_to_class(self, model_id: str) -> InferenceAPIModel:
+        if model_id == "gpt-4-base":
+            return self._openai_gpt4base  # NYU ARG is only org with access to this model
+        elif model_id in COMPLETION_MODELS:
+            return self._openai_completion
+        elif model_id in GPT_CHAT_MODELS or "ft:gpt-3.5-turbo" in model_id:
+            return self._openai_chat
+        elif model_id in ANTHROPIC_MODELS:
+            return self._anthropic_chat
+        raise ValueError(f"Invalid model id: {model_id}")
+
+    def filter_responses(
+        self,
+        candidate_responses: list[LLMResponse],
+        n: int,
+        is_valid: Callable[[str], bool],
+        insufficient_valids_behaviour: Literal["error", "continue", "pad_invalids"] = "error",
+    ) -> list[LLMResponse]:
+        # filter out invalid responses
+        num_candidates = len(candidate_responses)
+        valid_responses = [response for response in candidate_responses if is_valid(response.completion)]
+        num_valid = len(valid_responses)
+        success_rate = num_valid / num_candidates
+        if success_rate < 1:
+            LOGGER.info(f"`is_valid` success rate: {success_rate * 100:.2f}%")
+
+        # return the valid responses, or pad with invalid responses if there aren't enough
+        if num_valid < n:
+            match insufficient_valids_behaviour:
+                case "error":
+                    raise RuntimeError(f"Only found {num_valid} valid responses from {num_candidates} candidates.")
+                case "continue":
+                    responses = valid_responses
+                case "pad_invalids":
+                    invalid_responses = [
+                        response for response in candidate_responses if not is_valid(response.completion)
+                    ]
+                    invalids_needed = n - num_valid
+                    responses = [*valid_responses, *invalid_responses[:invalids_needed]]
+                    LOGGER.info(
+                        f"Padded {num_valid} valid responses with {invalids_needed} invalid responses to get {len(responses)} total responses"
+                    )
+        else:
+            responses = valid_responses
+        return responses[:n]
+
     async def __call__(
         self,
         model_ids: Union[str, list[str]],
@@ -96,19 +142,22 @@ class InferenceAPI:
                 cheapest model that has capacity. All models must be from the same class (e.g. OpenAI Base,
                 OpenAI Chat, or Anthropic Chat). Anthropic chat will error if multiple models are passed in.
                 Passing in multiple models could speed up the response time if one of the models is overloaded.
-            prompt: The prompt to send to the model(s). Type should match what's expected by the model(s).
+            prompt: The prompt to send to the model(s). Type should be Prompt.
             max_tokens: The maximum number of tokens to request from the API (argument added to
                 standardize the Anthropic and OpenAI APIs, which have different names for this).
             print_prompt_and_response: Whether to print the prompt and response to stdout.
             n: The number of completions to request.
             max_attempts_per_api_call: Passed to the underlying API call. If the API call fails (e.g. because the
                 API is overloaded), it will be retried this many times. If still fails, an exception will be raised.
-            num_candidates_per_completion: How many candidate completions to generate for each desired completion. n*num_candidates_per_completion completions will be generated, then is_valid is applied as a filter, then the remaining completions are returned up to a maximum of n.
+            num_candidates_per_completion: How many candidate completions to generate for each desired completion.
+                n*num_candidates_per_completion completions will be generated, then is_valid is applied as a filter,
+                then the remaining completions are returned up to a maximum of n.
             is_valid: Candiate completions are filtered with this predicate.
-            insufficient_valids_behaviour: What should we do if the remaining completions after applying the is_valid filter is shorter than n.
-                error: raise an error
-                continue: return the valid responses, even if they are fewer than n
-                pad_invalids: pad the list with invalid responses up to n
+            insufficient_valids_behaviour: What should we do if the remaining completions after applying the is_valid
+                filter is shorter than n.
+                - error: raise an error
+                - continue: return the valid responses, even if they are fewer than n
+                - pad_invalids: pad the list with invalid responses up to n
         """
 
         assert "max_tokens_to_sample" not in kwargs, "max_tokens_to_sample should be passed in as max_tokens."
@@ -123,18 +172,7 @@ class InferenceAPI:
             else:
                 model_ids = [model_ids]
 
-        def model_id_to_class(model_id: str) -> InferenceAPIModel:
-            if model_id == "gpt-4-base":
-                return self._openai_gpt4base  # NYU ARG is only org with access to this model
-            elif model_id in COMPLETION_MODELS:
-                return self._openai_completion
-            elif model_id in GPT_CHAT_MODELS or "ft:gpt-3.5-turbo" in model_id:
-                return self._openai_chat
-            elif model_id in ANTHROPIC_MODELS:
-                return self._anthropic_chat
-            raise ValueError(f"Invalid model id: {model_id}")
-
-        model_classes = [model_id_to_class(model_id) for model_id in model_ids]
+        model_classes = [self.model_id_to_class(model_id) for model_id in model_ids]
         if len(set(str(type(x)) for x in model_classes)) != 1:
             raise ValueError("All model ids must be of the same type.")
 
@@ -176,38 +214,21 @@ class InferenceAPI:
                 **kwargs,
             )
 
-        # filter out invalid responses
-        valid_responses = [response for response in candidate_responses if is_valid(response.completion)]
-        num_valid = len(valid_responses)
-        success_rate = num_valid / num_candidates
-        if success_rate < 1:
-            LOGGER.info(f"`is_valid` success rate: {success_rate * 100:.2f}%")
-
-        # return the valid responses, or pad with invalid responses if there aren't enough
-        if num_valid < n:
-            match insufficient_valids_behaviour:
-                case "error":
-                    raise RuntimeError(f"Only found {num_valid} valid responses from {num_candidates} candidates.")
-                case "continue":
-                    responses = valid_responses
-                case "pad_invalids":
-                    invalid_responses = [
-                        response for response in candidate_responses if not is_valid(response.completion)
-                    ]
-                    invalids_needed = n - num_valid
-                    responses = [*valid_responses, *invalid_responses[:invalids_needed]]
-                    LOGGER.info(
-                        f"Padded {num_valid} valid responses with {invalids_needed} invalid responses to get {len(responses)} total responses"
-                    )
-        else:
-            responses = valid_responses
+        # filter based on is_valid criteria and insufficient_valids_behaviour
+        responses = self.filter_responses(
+            candidate_responses,
+            n,
+            is_valid,
+            insufficient_valids_behaviour,
+        )
 
         # update running cost and model timings
-        self.running_cost += sum(response.cost for response in valid_responses)
-        for response in responses:
+        self.running_cost += sum(response.cost for response in candidate_responses)
+        for response in candidate_responses:
             self.model_timings.setdefault(response.model_id, []).append(response.api_duration)
             self.model_wait_times.setdefault(response.model_id, []).append(response.duration - response.api_duration)
-        return responses[:n]
+
+        return responses
 
     def reset_cost(self):
         self.running_cost = 0
